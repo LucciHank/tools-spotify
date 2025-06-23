59e3c32457fee79290076540191c371786229c8f
Your Name <hoanganhdo182@gmail.com>
2025-06-23 18:24:12 +0700

    Lần 3

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 config = {
     "uri":"https://spotify.tomoi.vn",
-    "redirect_uri":"https://spotify.tomoi.vn/login.html",
+    "redirect_uri":"https://spotify.tomoi.vn",
     "client_id":"21c4d7aedfaf484fa759730d9af1788f",
     "slowdown_import": 100,
     "slowdown_export": 100
